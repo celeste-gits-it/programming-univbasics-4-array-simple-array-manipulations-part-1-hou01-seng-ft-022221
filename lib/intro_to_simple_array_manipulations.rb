cdd1ad01430cf8b7d67bb87d154aaa7db3836117
Celeste Carter <celesteanell@gmail.com>
2021-02-08 18:47:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,4 @@
 def using_push(colors_in_the_rainbow, next_color)
-  colors_in_the_rainbow = ["red", "yellow"]
-  next_color = (blue)
-  colors_in_the_rainbow.push(next_color)
+  using_push = ["colors_in_the_rainbow"]
+  using_push.push(next_color)
 end

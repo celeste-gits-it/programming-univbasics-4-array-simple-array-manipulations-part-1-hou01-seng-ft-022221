2d0e45869a6369a7c380de89261631bf3661fb93
Celeste Carter <celesteanell@gmail.com>
2021-02-08 18:53:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,4 +3,8 @@ def using_push(colors_in_the_rainbow, next_color)
   using_push.push(next_color)
 end
 
-def using_unshift()
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
+  using_unshift = [bouroughs_in_nyc]
+  using_unshift.unshift(new_neighborhood)
+end
+

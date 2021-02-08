3536eb94491183cb5930e0bc59b5642e9778689b
Celeste Carter <celesteanell@gmail.com>
2021-02-08 19:38:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,12 +1,3 @@
-def using_push(array, string)
-  using_push = ["red", "yellow"]
-  array.push("violet")
-end
+def using_push(colors_of_)
 
-def using_unshift(array, string)
-  array.unshift ("Staten Island")
-end
-
-def using_pop(array) 
-    array.pop("North America")
 end

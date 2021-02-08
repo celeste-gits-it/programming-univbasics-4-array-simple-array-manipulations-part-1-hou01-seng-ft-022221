e6dea35d9057540036be6752b991f7815fb30f74
Celeste Carter <celesteanell@gmail.com>
2021-02-08 18:49:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,6 @@
 def using_push(colors_in_the_rainbow, next_color)
   using_push = ["colors_in_the_rainbow"]
-  using_push.push(next_color)
+  using_push.push("next_color")
 end
+
+def using_unshift(array, string)
